8faff50d0b640b37c04bef4dc23fce7caeb95b89
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-11-05 01:32:17 +0100

    create function to read json file from asset and return it's content as string/json which can be converted to json object or deserializes into an object of specified class using gson

--- app/src/main/java/com/hassan/parsingjson/MainActivity.kt
+++ app/src/main/java/com/hassan/parsingjson/MainActivity.kt
@@ -2,12 +2,43 @@ package com.hassan.parsingjson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import com.hassan.parsingjson.model.LatestRates
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("jsonTesting", loadJsonFromAsset().toString())
 
+    }
+
+    private fun loadJsonFromAsset(): LatestRates? {
+        val json: LatestRates?
+
+        try {
+            //use InputStream to open the file and stream the data into it.
+            val stream = assets.open("test_json_file.json")
+            //create a variable to store the size of the file.
+            val size = stream.available()
+            //create a buffer of the size of the file.
+            val buffer = ByteArray(size)
+            //read the inputStream file into the buffer.
+            stream.read(buffer)
+            //close the inputStream file.
+            stream.close()
+            //convert the buffer file to the format in which you need your data.
+            val stringJson = String(buffer, charset("UTF-8"))
+            val gson = Gson()
+            json = gson.fromJson(stringJson, LatestRates::class.java)
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+        return json
     }
 }
